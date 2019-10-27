65b1178bb04671e93b1fc7aefd5c97388c5be1cb
shamash <warp.buddhist@gmail.com>
2019-10-27 15:34:33 +0200

    Add doom config

---
+++ doom.d/config.el
@@ -0,0 +1,21 @@
+;;; .doom.d/config.el -*- lexical-binding: t; -*-
+
+;; Place your private configuration here
+
+(setq doom-theme 'doom-solarized-dark
+      lsp-auto-guess-root t
+      dart-format-on-save t)
+
+(add-hook 'dart-mode-hook 'lsp)
+(add-hook 'js2-mode-hook #'run-import-js)
+(add-hook 'typescript-mode-hook #'run-import-js)
+
+(with-eval-after-load "projectile"
+  (add-to-list 'projectile-project-root-files-bottom-up "pubspec.yaml")
+  (add-to-list 'projectile-project-root-files-bottom-up "BUILD"))
+
+(def-package! flutter :init (map! :map dart-mode-map :localleader "r r" #'flutter-run-or-hot-reload))
+
+(map! :ne "SPC m c a" #'lsp-execute-code-action)
+(map! :map js2-mode-map  :localleader "i f" #'import-js-fix)
+(map! :map typescript-mode-map  :localleader "i f" #'import-js-fix)
